ef5d3ad15e7de395ccc8c7fc76b4fdde0798583a
imani alexis <alexis.bailey37@gmail.com>
2018-10-18 11:37:43 -0400

    commiting to save remotely

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,64 +4,27 @@ class QuoteMachine extends Component {
     constructor() {
         super();
         this.state = {
-            quote: {
-                content: '',
-                title: ''
-            },
+            quote: ''
             hasQuote: false
         }
-        this.END_POINT ="https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
-        // this needs to be https otherwise you will run into errors
+        this.END_POINT ="http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
     }
     getRandomQuote = event => {
-        fetch(this.END_POINT)
-        .then(response => response.json())
-        .then(data => {
-            console.log('This is the data >>>', data[0].title, data[0].content)
-           if (data[0].content && data[0].title) {
-               let { quote } = this.state;
-               quote.content = data[0].content;
-               quote.title = data[0].title;
-               this.setState( { quote }, () => {
-                   if (this.state.hasQuote === false) {
-                       this.setState({ hasQuote: true })
-                   }
-               })
-            }
-             else {
-               return console.error('No quote has been found')
-           }
-        })
-    }
-
-    renderQuote = () => {
-        const { title, content } = this.state.quote;
-        return (
-            <div>
-                <h1> { title } </h1>
-                <p><i> { content } </i>
-                <hr />
-        )
+        console.log("it's clicking my guy ")
     }
     render () {
         
-        const { hasQuote,quote } = this.state;
+        const { hasQuote } = this.state
         return (
        <Fragment>
             <h1>Quote Machine </h1>
-            <button onClick={this.getRandomQuote}> 
-                Click me to get a random quote
-             </button>
-            {/* isMember ? "$2.00" : "$10.00" */}
-            <br />
-            { hasQuote === true ? 
-            this.renderQuote()
-            : 'no quote yet'}
+            <button onClick={this.getRandomQuote}>Click me to get a random quote</button>
+           { hasQuote === true : null 'no quote yet?'}
            </Fragment>
         )
     }
 
 }
 
-export default QuoteMachine;
+export default QuoteMachine
