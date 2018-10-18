022fb8f1e188ea0282c078f31786df74d26c5886
imani alexis <alexis.bailey37@gmail.com>
2018-10-18 12:45:28 -0400

    quotes are populating

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,8 +4,11 @@ class QuoteMachine extends Component {
     constructor() {
         super();
         this.state = {
-            quote: '',
-            hasQuote: false
+            quote: {
+                content: '',
+                title: ''
+            },
+            hasQuote: true
         }
         this.END_POINT ="https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
@@ -14,8 +17,30 @@ class QuoteMachine extends Component {
         fetch(this.END_POINT)
         .then(response => response.json())
         .then (data => {
+            if(data[0].content && data[0].title) {
+                let { quote } = this.state;
+                quote.content = data[0].content;
+                quote.title = data[0].title;
+                this.setState( { quote }, () => {
+                    if(this.setState.hasQuote === false) {
+                        this.setState( { hasQuote: true} )
+                    }
+                })
+            } else {
+                return console.error('No quote has been found 404')
+            }
             console.log(data)
         })
+    }
+    renderQuote = () => {
+        const { title, content } = this.state.quote;
+        return(
+            <div>
+                <h1> { title } </h1>
+                <p> { content } </p>
+
+                </div>
+        )
     }
     render () {
         
@@ -25,7 +50,9 @@ class QuoteMachine extends Component {
             <h1>Quote Machine </h1>
             <button onClick={this.getRandomQuote}>Click me to get a random quote</button>
            <br />
-           { hasQuote === true ? null : 'no quote yet?'}
+           { hasQuote === true ? 
+             this.renderQuote()
+             : 'no quote yet?'}
            </Fragment>
         )
     }
