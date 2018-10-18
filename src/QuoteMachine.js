3fe3a65afba17ca7b70ce1c4c97cc2d03191b429
imani alexis <alexis.bailey37@gmail.com>
2018-10-18 12:03:36 -0400

    able to console log quotes

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,23 +4,28 @@ class QuoteMachine extends Component {
     constructor() {
         super();
         this.state = {
-            quote: ''
+            quote: '',
             hasQuote: false
         }
-        this.END_POINT ="http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+        this.END_POINT ="https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
     }
     getRandomQuote = event => {
-        console.log("it's clicking my guy ")
+        fetch(this.END_POINT)
+        .then(response => response.json())
+        .then (data => {
+            console.log(data)
+        })
     }
     render () {
         
-        const { hasQuote } = this.state
+        const { hasQuote } = this.state;
         return (
        <Fragment>
             <h1>Quote Machine </h1>
             <button onClick={this.getRandomQuote}>Click me to get a random quote</button>
-           { hasQuote === true : null 'no quote yet?'}
+           <br />
+           { hasQuote === true ? null : 'no quote yet?'}
            </Fragment>
         )
     }
